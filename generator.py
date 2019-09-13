7a05d9b5cddcb2edc7d68d676346c77ce0f2fe25
chriscasano <chriscasano@hotmail.com>
2019-09-13 12:21:04 -0400

    Create generator.py

---
+++ generator.py
@@ -0,0 +1,27 @@
+#!/usr/local/bin/python
+
+import os, sys, csv
+import random
+from datetime import date, timedelta
+
+i=1;
+file = ""
+while 1==1:
+
+  tradeid = i
+  description = ''.join(random.sample('ABCDEFGHIJKLM1234567890@#',15))
+  tradedate = date.today()
+  settlementdate = tradedate + timedelta(days=5)
+  traderid = random.randint(1,100)
+  brokerid = random.randint(1,20)
+  cusip = ''.join(random.sample('ABCDEFGHIJKLM1234567890@#',9))
+  settlementamount = random.randint(1,999999999)
+  count = random.randint(1,1000000)
+
+  print tradeid.__str__() + "|" + description + "|" + tradedate.__str__() + "|" + settlementdate.__str__()  + "|" + tradeid.__str__()  + "|" + brokerid.__str__()   + "|" + cusip.__str__()  + "|" +  settlementamount.__str__()  + "|" +  count.__str__()
+  line = tradeid.__str__() + "|" + description + "|" + tradedate.__str__() + "|" + settlementdate.__str__()  + "|" + tradeid.__str__()  + "|" + brokerid.__str__()   + "|" + cusip.__str__()  + "|" +  settlementamount.__str__()  + "|" +  count.__str__()
+  """file = file + '\n' + line"""
+  i=i+1
+
+else:
+  print "done"
